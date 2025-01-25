75f1974fb82acbf64844121ed0a2d81e9fbcde74
spencer <spencer.m.owens@gmail.com>
2025-01-25 11:15:10 -0800

    configure routing for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,20 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
     },
   },
 })
